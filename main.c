24521161eacd43b96f6d8011e2a72cf23510dfdc
MKoussa <matthew.koussa@gmail.com>
2024-04-24 22:04:27 -0500

    Clean-up

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "userdelfx.h"
-#include "math.h"
 #include "buffer_ops.h"
 
 #define BUFFER_LEN 48000 * 2
@@ -19,39 +18,29 @@ void DELFX_PROCESS(float *xn, uint32_t frames)
 {
   for(uint32_t i = 0; i < frames; i++)
   {
-    // s_delay_ram[echoRate * 2]     = (xn[i * 2]     + s_delay_ram[echoRate * 2])     / 2;
-    // s_delay_ram[echoRate * 2 + 1] = (xn[i * 2 + 1] + s_delay_ram[echoRate * 2 + 1]) / 2;
-
-    // xn[i * 2]     = (s_delay_ram[BUFFER_LEN - (echoRate * 2)]     + xn[i * 2])     / 2;
-    // xn[i * 2 + 1] = (s_delay_ram[BUFFER_LEN - (echoRate * 2 + 1)] + xn[i * 2 + 1]) / 2;
-
     s_delay_ram[echoRate * 2]     = ((xn[i * 2]     + (s_delay_ram[echoRate * 2] * depth))     * 10000) / 20000;   
     s_delay_ram[echoRate * 2 + 1] = ((xn[i * 2 + 1] + (s_delay_ram[echoRate * 2 + 1] * depth)) * 10000) / 20000;
 
-    // xn[i * 2]     = ((s_delay_ram[BUFFER_LEN - (echoRate * 2)]     + xn[i * 2]) * 10000) / 20000;
-    // xn[i * 2 + 1] = ((s_delay_ram[BUFFER_LEN - (echoRate * 2 + 1)] + xn[i * 2 + 1]) * 10000) / 20000;
     xn[i * 2]     = (s_delay_ram[BUFFER_LEN - (echoRate * 2)]     + xn[i * 2])     / 2;
     xn[i * 2 + 1] = (s_delay_ram[BUFFER_LEN - (echoRate * 2 + 1)] + xn[i * 2 + 1]) / 2;
 
     echoRate++;
-    if(echoRate > (BUFFER_LEN / 2) - 2)
-    {
-      echoRate = 0;
-    }
+    if(echoRate > (BUFFER_LEN / 2) - 2) { echoRate = 0; }
   }
 }
 
 void DELFX_PARAM(uint8_t index, int32_t value)
 {
   const float valf = q31_to_f32(value);
-  switch (index) {
-  case k_user_delfx_param_time:
-    echoRate = (valf * ((BUFFER_LEN) / 2));
-    break;
-  case k_user_delfx_param_depth:
-    depth = valf;
-    break;
-  default:
-    break;
+  switch (index) 
+  {
+    case k_user_delfx_param_time:
+      echoRate = (valf * ((BUFFER_LEN) / 2));
+      break;
+    case k_user_delfx_param_depth:
+      depth = valf;
+      break;
+    default:
+      break;
   }
 }
