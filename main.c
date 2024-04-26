168012b6892e5c3b55b987b35da5731218aa8d72
MKoussa <matthew.koussa@gmail.com>
2024-04-25 21:59:26 -0500

    Add wet/dry mix, setting reverse length

--- main.c
+++ main.c
@@ -1,31 +1,43 @@
 #include "userdelfx.h"
 #include "buffer_ops.h"
 
-#define BUFFER_LEN 48000 * 2
+#define BUFFER_LEN 48000 * 4
+#define BUFFER_LEN_HALF 48000
+
 static __sdram float s_delay_ram[BUFFER_LEN];
 
-static float depth;
-static uint32_t echoRate;
+static float depth, wetDry;
+static uint32_t echoRate, echoMax;
 
 void DELFX_INIT(uint32_t platform, uint32_t api)
 {
   buf_clr_f32(s_delay_ram, BUFFER_LEN);
   depth = 0.0f;
   echoRate = 0;
+  wetDry = 0.0f;
+  echoMax = 0;
 }
 
 void DELFX_PROCESS(float *xn, uint32_t frames)
 {
   for(uint32_t i = 0; i < frames; i++)
   {
-    s_delay_ram[echoRate * 2]     = ((xn[i * 2]     + (s_delay_ram[echoRate * 2] * depth))     * 10000) / 20000;   
-    s_delay_ram[echoRate * 2 + 1] = ((xn[i * 2 + 1] + (s_delay_ram[echoRate * 2 + 1] * depth)) * 10000) / 20000;
+    s_delay_ram[echoRate * 2]     = (xn[i * 2]     + (s_delay_ram[echoRate * 2]     * depth)) / (0.99 + depth);   
+    s_delay_ram[echoRate * 2 + 1] = (xn[i * 2 + 1] + (s_delay_ram[echoRate * 2 + 1] * depth)) / (0.99 + depth);
 
-    xn[i * 2]     = (s_delay_ram[BUFFER_LEN - (echoRate * 2)]     + xn[i * 2])     / 2;
-    xn[i * 2 + 1] = (s_delay_ram[BUFFER_LEN - (echoRate * 2 + 1)] + xn[i * 2 + 1]) / 2;
+    if(wetDry > 0)
+    {
+      xn[i * 2]     = (xn[i * 2]     + (s_delay_ram[(echoMax * 2)     - (echoRate * 2)]     * wetDry)) / (1 + wetDry);
+      xn[i * 2 + 1] = (xn[i * 2 + 1] + (s_delay_ram[(echoMax * 2 + 1) - (echoRate * 2 + 1)] * wetDry)) / (1 + wetDry);
+    }
+    else
+    {
+      xn[i * 2]     = xn[i * 2];
+      xn[i * 2 + 1] = xn[i * 2 + 1];
+    }
 
     echoRate++;
-    if(echoRate > (BUFFER_LEN / 2) - 2) { echoRate = 0; }
+    if(echoRate > echoMax) { echoRate = 0; }
   }
 }
 
@@ -35,10 +47,14 @@ void DELFX_PARAM(uint8_t index, int32_t value)
   switch (index) 
   {
     case k_user_delfx_param_time:
-      echoRate = (valf * ((BUFFER_LEN) / 2));
+      echoMax = (valf * BUFFER_LEN_HALF) + 48000;
       break;
     case k_user_delfx_param_depth:
+      echoRate = 0;
       depth = valf;
+      break;
+    case k_user_delfx_param_shift_depth:
+      wetDry = valf;
       break;
     default:
       break;
